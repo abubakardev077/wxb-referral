ee55ce9a3b1d325bd7cce30df8f10e58fd1d1208
Ideacloud-ng <sqaleup@gmail.com>
2023-01-07 16:40:20 +0000

    add

--- server/model/auth.model.js
+++ server/model/auth.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
     addressReferred: [
       {
         address: String,
-        isMinted: {type: Boolean, default: false}
+        mintType: String
       },
     ],
   },
